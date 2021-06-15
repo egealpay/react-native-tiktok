384c4fe05d44aa8de66ce4753bfa77033f9111b3
egealpay <neo.1Zencig1thu6>
2021-06-15 16:42:09 +0300

    Init: Empty Project

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App: () => Node = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
