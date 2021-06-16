5e933ce9951a317aff73059990e99348f38f8d6d
egealpay <neo.1Zencig1thu6>
2021-06-16 19:32:40 +0300

    Add: Top Bar

--- App/index.js
+++ App/index.js
@@ -20,9 +20,81 @@ import StyleConstants from './styles';
 import CreateButton from './components/create-button';
 import InboxScreenContainer from './screens/inbox/inbox-screen-container';
 import MeScreenContainer from './screens/me/me-screen-container';
+import {createStackNavigator} from '@react-navigation/stack';
 
 
 const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const DiscoverStack = createStackNavigator();
+const CreateStack = createStackNavigator();
+const InboxStack = createStackNavigator();
+const MeStack = createStackNavigator();
+
+
+function meStack({navigation}) {
+    return <MeStack.Navigator>
+        <MeStack.Screen
+            name="MeStack"
+            component={MeScreenContainer}
+            options={{
+                title: 'Profile',
+                headerRight: () => (
+                    <MaterialCommunityIcons
+                        name={'dots-vertical'}
+                        size={24}
+                        style={{marginRight: 16}}
+                    />
+                ),
+            }}
+        />
+    </MeStack.Navigator>;
+}
+
+function inboxStack({navigation}) {
+    return <InboxStack.Navigator>
+        <InboxStack.Screen
+            name="InboxStack"
+            component={InboxScreenContainer}
+            options={{title: 'All Activity'}}
+        />
+    </InboxStack.Navigator>;
+}
+
+function createStack({navigation}) {
+    return <CreateStack.Navigator
+        screenOptions={{
+            headerShown: false,
+        }}>
+        <CreateStack.Screen
+            name="CreateStack"
+            component={HomeScreenContainer}
+        />
+    </CreateStack.Navigator>;
+}
+
+function discoverStack({navigation}) {
+    return <DiscoverStack.Navigator
+        screenOptions={{
+            headerShown: false,
+        }}>
+        <DiscoverStack.Screen
+            name="DiscoverStack"
+            component={HomeScreenContainer}
+        />
+    </DiscoverStack.Navigator>;
+}
+
+function homeStack({navigation}) {
+    return <HomeStack.Navigator
+        screenOptions={{
+            headerShown: false,
+        }}>
+        <HomeStack.Screen
+            name="HomeStack"
+            component={HomeScreenContainer}
+        />
+    </HomeStack.Navigator>;
+}
 
 const App: () => Node = () => {
     useEffect(() => [
@@ -56,12 +128,12 @@ const App: () => Node = () => {
                     inactiveTintColor: StyleConstants.SPANISH_GRAY,
                     style: {backgroundColor: StyleConstants.WHITE, borderTopWidth: 0},
                 }}>
-                <Tab.Screen name="Home" component={HomeScreenContainer}/>
-                <Tab.Screen name="Discover" component={HomeScreenContainer}/>
-                <Tab.Screen name="Create" component={HomeScreenContainer}
+                <Tab.Screen name="Home" component={homeStack}/>
+                <Tab.Screen name="Discover" component={discoverStack}/>
+                <Tab.Screen name="Create" component={createStack}
                             options={{title: '', tabBarIcon: (props) => <CreateButton/>}}/>
-                <Tab.Screen name="Inbox" component={InboxScreenContainer}/>
-                <Tab.Screen name="Me" component={MeScreenContainer}/>
+                <Tab.Screen name="Inbox" component={inboxStack}/>
+                <Tab.Screen name="Me" component={meStack}/>
             </Tab.Navigator>
         </NavigationContainer>
     </SafeAreaProvider>;
